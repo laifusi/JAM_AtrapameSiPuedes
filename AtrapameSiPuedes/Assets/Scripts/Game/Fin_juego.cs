c82555863d5bbb927ddce9addf5cb4fb78ab8b6c
Laia <laia.fustercorral@gmail.com>
2022-09-24 20:46:12 +0200

    Corrección usos del Game_Controller

--- AtrapameSiPuedes/Assets/Scripts/Game/Fin_juego.cs
+++ AtrapameSiPuedes/Assets/Scripts/Game/Fin_juego.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class Fin_juego : MonoBehaviour
 {
-    public Game_Controller gc;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             //Derrota
-            gc.Derrota();
+            Game_Controller.Instance.Derrota();
         }
     }
 }
